33567031cf0b998a7b8b4baaaa05a7a51d5e3cac
ealex <ealex@br.ibm.com>
2020-03-03 18:52:53 -0300

    Versao atual

---
+++ app/src/main/java/com/mobile/vencarro/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.mobile.vencarro
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        supportActionBar?.hide() // ocultar a barra de titulo
+
+    }
+}
